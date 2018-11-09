23a3f4d13972418b3343794472de91d43037988c
Joonas Ylitalo <ylitalo.joonas@gmail.com>
2018-11-09 13:52:22 +0200

    Without shallowclonse stuff broke, necessary tweaks for new shipit version

--- generators/app/templates/typo3/_shipitfile.js
+++ generators/app/templates/typo3/_shipitfile.js
@@ -2,52 +2,70 @@
  * Deployment
  * ======================================== */
 
-require('dotenv').config()
-const pkg = require('./package.json')
-const WEBROOT = '/var/www/webroot/ROOT'
-const WORKSPACE = process.env.WORKSPACE || '/tmp/<%= dir %>'
+require('dotenv').config();
+const pkg = require('./package.json');
+const WEBROOT = process.env.PRODUCTION_WEBROOT;
+const WORKSPACE = process.env.WORKSPACE || '/tmp/<%= dir %>';
 
-module.exports = shipit => {
-  require('shipit-deploy')(shipit)
+module.exports = (shipit) => {
+  require('shipit-deploy')(shipit);
 
   shipit.initConfig({
     default: {
       workspace: WORKSPACE,
       repositoryUrl: pkg.repository,
-      ignores: [".*", "gulpfile.js", "package.json", "README.md", "rev-manifest.json", "shipitfile.js", "./Assets/", "node_modules/", "selection.json", "yarn.lock"],
+      ignores: [
+        '.*',
+        'gulpfile.js',
+        'package.json',
+        'README.md',
+        'rev-manifest.json',
+        'shipitfile.js',
+        './Assets/',
+        'node_modules/',
+        'selection.json',
+        'yarn.lock',
+      ],
       keepReleases: 5,
       deleteOnRollback: false,
       rsync: ['--include="dist/**/*"'],
+      shallowClone: false,
     },
     production: {
       branch: 'master',
       deployTo: `${WEBROOT}/typo3conf/releases/<%= dir %>`,
       servers: process.env.PRODUCTION_SSH,
-    }
-  })
+    },
+  });
 
-  shipit.blTask('npm', () => {
-    shipit.log('Installing dependencies...')
-    return shipit.local('npm install', { cwd: WORKSPACE })
+  shipit.on('init', () => {
+    return shipit.local(`mkdir -p ${WORKSPACE}`);
+  });
+
+  shipit.blTask('install', () => {
+    shipit.log('Installing dependencies...');
+    return shipit
+      .local('yarn', { cwd: WORKSPACE })
       .then(() => shipit.log('Successfully installed dependencies'))
-      .catch(() =>  {
-        shipit.log('Failed to install dependencies')
-        process.exit()
-      })
-  })
+      .catch(() => {
+        shipit.log('Failed to install dependencies');
+        process.exit();
+      });
+  });
 
   shipit.blTask('build', () => {
-    shipit.log('Running build...')
-    return shipit.local('npm run build', { cwd: WORKSPACE })
+    shipit.log('Running build...');
+    return shipit
+      .local('yarn build', { cwd: WORKSPACE })
       .then(() => shipit.log('Build successful'))
       .catch(() => {
-        shipit.log('Build failed')
-        process.exit()
-      })
-  })
+        shipit.log('Build failed');
+        process.exit();
+      });
+  });
 
   shipit.blTask('setup', () => {
-    shipit.log('Running setup...')
+    shipit.log('Running setup...');
     shipit.remote(`
       if [ ! -d "${WEBROOT}/typo3conf/ext/<%= dir %>" ]
         then
@@ -57,13 +75,13 @@ module.exports = shipit => {
             ${WEBROOT}/typo3conf/ext/<%= dir %>
       fi
     `);
-  })
+  });
 
   shipit.on('fetched', () => {
-    shipit.start('npm', 'build')
-  })
+    shipit.start('install', 'build');
+  });
 
   shipit.on('published', () => {
-    shipit.start('setup')
-  })
-}
+    shipit.start('setup');
+  });
+};
