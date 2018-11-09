8d95091b0f31e86e3edc4b5cc326fb09605db870
Joonas Ylitalo <ylitalo.joonas@gmail.com>
2018-11-09 13:38:24 +0200

    Remove unnecessary and soon to be unnecessary stuff

--- generators/app/templates/shared/assets/_app.head.js
+++
@@ -1,8 +0,0 @@
-/* ========================================
- * Application head
- * ======================================== */
-
-/**
- * Global custom build modernizr
- */
-import '<% if (html) { %>../../dist/assets<% } if (typo3) { %>../Resources/Public/Assets<% } if (wp) { %>../dist/assets<% } %>/vendors/modernizr.js' // eslint-disable-line
