423cf90ddd5937829bc3a974adfc361bcc25ecb3
Joonas Ylitalo <ylitalo.joonas@gmail.com>
2020-01-15 01:54:41 +0200

    Load required plugins automatically

--- generators/app/templates/wordpress/lib/plugins.php
+++ generators/app/templates/wordpress/lib/plugins.php
@@ -1,15 +1,34 @@
 <?php
 /* =======================================
- * Various plugin related tweaks
+ * Plugins
  * ======================================= */
+
+include_once (ABSPATH . 'wp-admin/includes/plugin.php');
+
+/**
+ * Automatically activate required plugins
+ */
+foreach ([
+    'timber-library/timber.php',
+    'advanced-custom-fields-pro/acf.php',
+    'sitepress-multilingual-cms/sitepress.php',
+] as $plugin) {
+    if (!is_plugin_active($plugin)) {
+        activate_plugin($plugin);
+
+        add_action('admin_notices', function() use ($plugin) {
+            echo '<div class="updated"><p>' . sprintf('<strong>%s</strong> plugin is required and auto-enabled by the current theme.', $plugin) . '</p></div>';
+        });
+    }
+}
 
 /**
  * Simple History: Reduce visibility
  */
-add_filter('simple_history/view_history_capability', function($cabability) {
-    $capability = 'manage_options';
-    return $capability;
-});
+add_filter('simple_history/view_history_capability', function($cap) {
+    $cap = 'manage_options';
+    return $cap;
+});<% if (pluginWPML) { %>
 
 /**
  * WPML: Remove metas and unneeded assets
@@ -25,4 +44,4 @@ if (!empty($GLOBALS['sitepress'])) {
     define('ICL_DONT_LOAD_NAVIGATION_CSS', true);
     define('ICL_DONT_LOAD_LANGUAGE_SELECTOR_CSS', true);
     define('ICL_DONT_LOAD_LANGUAGES_JS', true);
-}
+}<% } %>
