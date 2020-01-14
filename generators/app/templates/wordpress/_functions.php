0a10b1dfbe2bace19232beac38899dcbf3e3f1ee
Joonas Ylitalo <ylitalo.joonas@gmail.com>
2020-01-15 01:58:37 +0200

    Import all the new stuff, add twig funcs/filters

--- generators/app/templates/wordpress/_functions.php
+++ generators/app/templates/wordpress/_functions.php
@@ -3,59 +3,77 @@
  * Functions
  * ======================================= */
 
-if (!class_exists('Timber') || !function_exists('acf')<% if (pluginWPML) { %> || empty($GLOBALS['sitepress'])<% } %>) {
-    add_action('admin_notices', function() {
-        echo '<div class="error"><p>Timber<% if (pluginWPML) { %>, ACF or WPML<% } else { %> or AFC<% } %> not activated. Make sure you activate
-        the plugins in <a href="' . esc_url(admin_url('plugins.php')) . '">' .
-        esc_url(admin_url('plugins.php')) . '</a></p></div>';
-    });
-    return;
-}
+require __DIR__ . '/lib/plugins.php';
+require __DIR__ . '/lib/utility.php';
 
-
-/* ======
- * Setup application
- * ====== */
-
-Timber::$dirname = array('containers', 'components', 'templates', 'partials');
+Timber::$dirname = array('containers', 'components', 'partials', 'templates');
 
 class App extends TimberSite {
     function __construct() {
-        add_action('init', array($this, 'register_post_types'));
-        add_action('init', array($this, 'register_shortcodes'));
-        add_filter('timber_context', array($this, 'add_to_context'));
-        add_action('wp_enqueue_scripts', array($this, 'scripts'), '1');
-
         $this->setup();
+        add_action('init', [$this, 'register_post_types']);
+        add_action('init', [$this, 'register_shortcodes']);
+        add_filter('timber_context', [$this, 'add_to_context']);
+        add_filter('timber/twig', [$this, 'add_to_twig']);
+        add_action('wp_enqueue_scripts', [$this, 'scripts'], '1');
         parent::__construct();
     }
 
-    /**
-     * Timber context
-     */
+
+    /* ======
+     * Basics
+     * ====== */
+
+    function setup() {
+        require_once locate_template('/lib/acf.php');
+        require_once locate_template('/lib/clean-up.php');
+        require_once locate_template('/lib/gutenberg.php');
+        require_once locate_template('/lib/roles.php');
+        require_once locate_template('/lib/setup.php');
+    }
+
     function add_to_context($context) {
         $context['WP_DEV'] = getenv('WORDPRESS_ENV') == 'development';
-        $context['site'] = $this;
+        $context['lang'] = ICL_LANGUAGE_CODE;
+        $context['nav_primary'] = new TimberMenu('nav-primary');
         $context['options'] = get_fields('option');
-
-        $context['nav_primary'] = new TimberMenu('nav-primary');<% if (pluginWPML) { %>
-
-        $context['lang'] = ICL_LANGUAGE_CODE;<% } %>
+        $context['site'] = $this;
 
         return $context;
     }
 
-    /**
-     * Custom post types and taxonomies
-     * require_once locate_template('/lib/custom-post-types/cpt.php');
-     */
-    function register_post_types() {}
+    function register_post_types() {
+       require_once locate_template('/lib/custom-post-types/shared-components.php');
+    }
 
-    /**
-     * Shortcodes
-     * require_once locate_template('/lib/shortcodes/name.php');
-     */
-    function register_shortcodes() {}
+    function register_shortcodes() {
+        // require_once locate_template('/lib/shortcodes/name.php');
+    }
+
+
+    /* ======
+     * Extend Twig
+     * ====== */
+
+    function add_to_twig($twig) {
+        $twig->addFunction(new Timber\Twig_Function('display_template_file', [$this, 'display_template_file']));
+        $twig->addFilter(new Timber\Twig_Filter('merge_object', [$this, 'merge_object']));
+        return $twig;
+    }
+
+    function display_template_file() {
+        if (getenv('WORDPRESS_ENV') == 'development') {
+            global $template;
+            $parts = explode('/', $template);
+            $file = end($parts);
+            return '<small>Current template: ' . $file . '</small>';
+        }
+    }
+
+    function merge_object($origObj, $newObj) {
+        $merged = (object) array_merge((array) $origObj, (array) $newObj);
+        return $merged;
+    }
 }
 
 new App();
