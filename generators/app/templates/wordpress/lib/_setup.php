206eb34010b854b9198a7216995c7cdc3561b3f3
Joonas Sandell <me@joonassandell.com>
2020-08-31 15:13:08 +0300

    Add proper ratiobased image sizes

--- generators/app/templates/wordpress/lib/_setup.php
+++ generators/app/templates/wordpress/lib/_setup.php
@@ -24,16 +24,19 @@ add_action('after_setup_theme', function() {
     /**
      * Add additional images
      */
-    add_image_size('landscape_l', 1920, 960, true);
+    add_image_size('landscape_l', 1920, 1080, true);
     add_image_size('landscape_m', 1280, 720, true);
-    add_image_size('landscape_s', 640, 360, true);
+    add_image_size('landscape_s', 768, 432, true);
     add_image_size('landscape_xs', 384, 216, true);
+    add_image_size('portrait_s', 192, 342, true);
 
     /**
      * Create cropped default image sizes
      */
     add_image_size('large_crop', 960, 960, true);
+    add_image_size('medium_crop', 432, 432, true);
     set_post_thumbnail_size(168, 168, ['center', 'center']);
+
 
     /**
      * Make theme available for translation
